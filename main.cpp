455e2012330fb7cc50effe949c27e1e442822c71
technicalace <arguello.aj256@gmail.com>
2025-12-14 19:07:20 -0600

    Test

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+void currentGrade() {
+    // 1 - ask user for the number of different assignment types included in course (homeworks, quizzes, exams, etc.) (sort by name)
+    // 2 - for however many the user inputs, loop through one by one and ask the user to input the weight of each assigment type
+    // 3 - ask the user how many of each assignment type are within the course
+    
+    // run below for each individual assignment type 
+    // ask user how many assignments within that assignment type have been completed
+    // for each assignment type, ask what grade they got (ex. 20/20, 12/20, etc.) - run another function written to calculate based off input 
+
+    // output current grade to user based off information given
+
+    int assignmentType = 0; 
+    int assignments[assignmentType];
+    // 1 
+    cout << "Insert the number of assignment types in your course:\n";
+    cin >> assignmentType; 
+    // 2 
+    /*for (int i = 0; i < assignments.size(); i++) { // 
+        cout << "hello" << endl; 
+    }*/
+}
+
+int main() {
+    /* 
+    ask user if they would like generate current grade (1) or potential grade (2). 
+    if 1 is input, run currentGrade on main. 
+    */ 
+   cout << "Hello World" << endl; 
+   currentGrade();
+}
