12cd89d76c080cd71347c5403c442387d659b4a9
technicalace <arguello.aj256@gmail.com>
2025-12-18 18:14:43 -0600

    rough draft is almost complete, edge cases need to be considered

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <map>
 
 using namespace std;
 
@@ -21,21 +20,19 @@ void currentGrade() {
 
     double weightedSum = 0.0; 
     double totalWeight = 0.0; 
-    double sumOfGrades = 0; 
     double assignmentWeightsInput; 
 
-    map<string, double> assignmentsList;
     // data lists
     vector<string> assignmentTypesList; // keys  
     vector<int> assignmentTypeAmountList; // amount of assignments per type in the course
     vector<double> gradesByCatList; 
-    vector<double> gradesSum; 
     vector<double> assignmentWeightsList; // values 
     // 1 
-
+    cout << "\n\n\n==========INSERT ASSIGNMENTS AND THEIR EQUIVALENET WEIGHTS==========" << endl; 
     while (assignmentTypeInput != "e") { // while the user input for assignment type is not 'e'
         // assignment types 
-        cout << "Insert the assignment type (type e to exit): " << endl;
+        cout << "--------------------------------------------------------------------" << endl;
+        cout << "Insert the assignment type (quiz, test, homework, etc.) type e to exit: ";
         cin >> assignmentTypeInput; // insert input for assignment type 
 
         if (assignmentTypeInput == "e") { // if user input is 'e', break out of loop 
@@ -44,19 +41,21 @@ void currentGrade() {
 
         assignmentTypesList.push_back(assignmentTypeInput); // push back input into assignmentTypes vector 
         // assignment weights
-        cout << "Insert it's equivalent weight (in percentages): " << endl; 
+        cout << "Insert it's equivalent weight (in percentages): "; 
         cin >> assignmentWeightsInput; // insert input for assignment weights 
         assignmentWeightsList.push_back(assignmentWeightsInput); // push back input into assignmentWeights vector
     }
 
-    cout << "-----------------" << endl; 
-
+    cout << "\n\n\n==========INSERT FREQUENCY OF EACH ASSIGNMENT TYPE==========" << endl; 
     // 3 - ask the user how many of each assignment have been done so far 
     for (int i = 0; i < assignmentTypesList.size(); i++) {
+        cout << "--------------------------------------------------------------------" << endl;
         cout << "How many " << assignmentTypesList[i] << "\'s have been done so far in this course: "; 
         cin >> assignmentTypeAmount; 
         assignmentTypeAmountList.push_back(assignmentTypeAmount);
     }
+
+    cout << "\n\n\n==========INSERT SCORES FOR EACH ASSIGNMENT DONE==========" << endl; 
     // for each assignment type, ask what grade they got (ex. 20/20, 12/20, etc.) - run another function written to calculate based off input 
     for (int i = 0; i < assignmentTypesList.size(); i++) {
         double actualPoints = 0; // the points earned 
@@ -71,11 +70,12 @@ void currentGrade() {
         
         cout << "For " << assignmentTypesList[i] << "\'s..." << endl; 
         for (int j = 0; j < assignmentTypeAmountList[i]; j++) {
-            cout << "What was the total possible points for " << assignmentTypesList[i] << j << ": " << endl; 
+            cout << "--------------------------------------------------------------------" << endl;
+            cout << "What was the total possible points for " << assignmentTypesList[i] << " " << j + 1 << ": "; 
             cin >> input; 
             totalPoints += input; // add on the input into totalPoints 
 
-            cout << "What was the points earned for " << assignmentTypesList[i] << j << ": " << endl; 
+            cout << "What was the points earned for " << assignmentTypesList[i] << j << ": "; 
             cin >> input;
             actualPoints += input; // add on the input into actualPoints
 
@@ -84,15 +84,14 @@ void currentGrade() {
         }
         weightedSum += gradesByCatList[i] * assignmentWeightsList[i]; 
         totalWeight += assignmentWeightsList[i];
-        /*gradePerCat = assignmentWeightsList[i] * actualPoints / totalPoints; // calculate the i'th catagory's grade
-        gradesByCatList.push_back(gradePerCat); */
     }
 
-    cout << "-----------------" << endl; 
+    cout << "\n\n\n==========CURRENT OVERALL GRADE PRECENTAGE==========" << endl;
 
     double finalGrade = weightedSum / totalWeight;
     
     cout << "Total Grade: " << finalGrade * 100 << "%" << endl; 
+
 }
 
 int main() {
